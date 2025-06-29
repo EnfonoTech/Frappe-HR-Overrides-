403e53da92940506b2781fbfbb87624d28110b28
Ramees-Enfono <ramees@enfono.in>
2025-06-29 09:02:29 +0530

    first commit

---
+++ frontend/src/views/video/Dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+	<BaseLayout :pageTitle="__('Salary Slips')">
+		<template #body>
+			  <div class="video-page">
+				<h1>Welcome to the Video Page</h1>
+				<iframe
+				width="560"
+				height="315"
+				src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+				title="YouTube video"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+				allowfullscreen
+				></iframe>
+			</div>
+		</template>
+	</BaseLayout>
+</template>
+
+<script setup>
+
+import BaseLayout from "@/components/BaseLayout.vue"
+
+</script>
+<style scoped>
+.video-page {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
